6c0d87ad4f3772f72749dc30ed0ea2bec37e1f20
Xose Pérez <xose.perez@gmail.com>
2017-01-29 22:27:51 +0100

    Added preliminary support for multiple DS18B20 devices

--- code/espurna/ds18b20.ino
+++ code/espurna/ds18b20.ino
@@ -27,26 +27,86 @@ double _dsTemperature = 0;
 std::vector<DallasTemperature *> _ds18b20s;
 
 unsigned int createDS18B20(unsigned int pin) {
-    OneWire * wire = new OneWire(DS_PIN);
+
+    OneWire * wire = new OneWire(pin);
     DallasTemperature * ds18b20 = new DallasTemperature(wire);
     ds18b20->begin();
+
+    #ifdef DEBUG_PORT
+        DEBUG_MSG("[SENSORS] GPIO %d, found %d DS18B20 devices\n", pin, ds18b20->getDeviceCount());
+        DEBUG_MSG("[SENSORS] GPIO %d, DS18B20 parasite power %s\n", pin, ds18b20->isParasitePowerMode() ? "ON" : "OFF");
+        DeviceAddress address;
+        for (int i=0; i<ds18b20->getDeviceCount(); i++) {
+            ds18b20->getAddress(address, i);
+            DEBUG_MSG("[SENSORS] GPIO %d, DS18B20 #%d address: ", pin, i);
+            for (uint8_t i = 0; i < 8; i++) {
+                DEBUG_PORT.printf("%02X", address[i]);
+            }
+            DEBUG_MSG("\n");
+        }
+    #endif
+
     _ds18b20s.push_back(ds18b20);
     return _ds18b20s.size() - 1;
+
 }
+
+bool getDSAddress(uint8_t * address, unsigned int index) {
+    unsigned int aggregated = 0;
+    unsigned connectionId = 0;
+    while (connectionId <= _ds18b20s.size()) {
+        unsigned int count = _ds18b20s[connectionId]->getDeviceCount();
+        if (aggregated + count > index) {
+            _ds18b20s[connectionId]->getAddress(address, index - aggregated);
+            return true;
+        }
+        ++connectionId;
+    }
+    return false;
+}
+
+double getDSTemperature(unsigned int connectionId, unsigned int index) {
+    if (0 <= connectionId && connectionId < _ds18b20s.size()) {
+        _ds18b20s[connectionId]->requestTemperatures();
+        return _ds18b20s[connectionId]->getTempCByIndex(index);
+    }
+    return DEVICE_DISCONNECTED_C;
+}
+
+double getDSTemperatureByAddress(uint8_t * address) {
+    for (int i=0; i<_ds18b20s.size(); i++) {
+        _ds18b20s[i]->requestTemperatures();
+        double t = _ds18b20s[i]->getTempC(address);
+        if (t != DEVICE_DISCONNECTED_C) return t;
+    }
+    return DEVICE_DISCONNECTED_C;
+}
+
 
 double getDSTemperature(unsigned int index) {
-    if (0 <= index && index < _ds18b20s.size()) {
-        _ds18b20s[index]->requestTemperatures();
-        return _ds18b20s[index]->getTempCByIndex(0);
+    unsigned int aggregated = 0;
+    unsigned connectionId = 0;
+    while (connectionId <= _ds18b20s.size()) {
+        unsigned int count = _ds18b20s[connectionId]->getDeviceCount();
+        if (aggregated + count > index) {
+            return getDSTemperature(connectionId, index - aggregated);
+        }
+        ++connectionId;
     }
-    return 0;
+    return DEVICE_DISCONNECTED_C;
 }
 
-unsigned int getDHTCount() {
-    return _ds18b20s.size();
+double getDSTemperature() {
+    return getDSTemperature(0, 0);
 }
 
-double getDSTemperature() { return getDSTemperature(0); }
+unsigned int getDSCount() {
+    int count = 0;
+    for (int i=0; i<_ds18b20s.size(); i++) {
+        count += _ds18b20s[i]->getDeviceCount();
+    }
+    return count;
+}
 
 // -----------------------------------------------------------------------------
 // Setup & Loop
@@ -75,7 +135,7 @@ void dsLoop() {
         double t = getDSTemperature(0);
 
         // Check if readings are valid
-        if (isnan(t)) {
+        if (t == DEVICE_DISCONNECTED_C) {
 
             DEBUG_MSG("[DS18B20] Error reading sensor\n");
 
