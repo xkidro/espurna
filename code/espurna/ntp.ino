abd63340895c87b84506a6bb9a1602a55424ccd3
Xose Pérez <xose.perez@gmail.com>
2017-02-06 01:11:25 +0100

    Force NTP reconfiguration after 5 errors

--- code/espurna/ntp.ino
+++ code/espurna/ntp.ino
@@ -22,15 +22,24 @@ void ntpConnect() {
 void ntpSetup() {
 
     NTP.onNTPSyncEvent([](NTPSyncEvent_t error) {
+
+        static unsigned char errors = 0;
+
         if (error) {
             if (error == noResponse) {
                 DEBUG_MSG("[NTP] Error: NTP server not reachable\n");
+                if (++errors == 5) {
+                    errors = 0;
+                    ntpConnect();
+                }
             } else if (error == invalidAddress) {
                 DEBUG_MSG("[NTP] Error: Invalid NTP server address\n");
             }
         } else {
+            errors = 0;
             DEBUG_MSG("[NTP] Time: %s\n", (char *) NTP.getTimeDateString(NTP.getLastNTPSync()).c_str());
         }
+
     });
 
 }
