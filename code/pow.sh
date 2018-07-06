452cf1fbac5aacd996b0209f085655a243757a51
Cristian Antal <xkid@xkid.ro>
2018-07-06 16:38:23 +0300

    updated script

--- code/pow.sh
+++ code/pow.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-PLATFORMIO_BUILD_FLAGS="'-DUSE_CUSTOM_H'" ./build.sh itead-sonoff-pow
+# Update remote
+git remote update
+
+# Start checking
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+	echo "No need to update"
+elif [ $LOCAL = $BASE ]; then
+	PLATFORMIO_BUILD_FLAGS="'-DUSE_CUSTOM_H'" ./build.sh itead-sonoff-pow
+fi
